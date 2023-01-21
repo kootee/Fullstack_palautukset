f4865d2e0e6b78797c2c873f72897f48fd01bd44
kootee <katjatoivola@icloud.com>
2023-01-21 21:57:02 +0200

    app.js päivitetty

--- 2/maiden_tiedot/src/App.js
+++ 2/maiden_tiedot/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Show = ({ toShow, searchKey}) => {
+  if (searchKey.length === 0 || toShow.length > 10) {
+    return <div>Too many matches, specify another filter</div>
+  }
+  return (  
+    <div>
+      {toShow.map(country =>
+        <ListCountries key={country.cca2} name={country.name}/> 
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+const ListCountries = ({name}) => {
+  return (
+    <div>
+      <li>{name.common}</li>
+    </div>
+  )
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [searchKey, setKey] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+    .get('https://restcountries.com/v3.1/all')
+    .then(response => {
+      setCountries(response.data)
+      console.log('promise fulfilled')
+    })
+  }
+
+  useEffect(hook, [])
+
+  const handleChange = (event) => {
+    console.log('name change', event.target.value)
+    setKey(event.target.value)
+  }
+
+  const filterCountries = () => {
+    console.log('filtering countries by', searchKey)
+    const filtered =  countries.filter(country => country.name.common.includes(searchKey))
+    console.log('countries that were filtered', filtered)
+
+    return filtered
+  }
+
+  return(
+    <div>
+      <h2>Countries</h2>
+        <div>name: <input
+          value={searchKey}
+          onChange={handleChange}
+        /></div>
+      <ul>
+        <Show toShow={filterCountries()} searchKey={searchKey}/>
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
